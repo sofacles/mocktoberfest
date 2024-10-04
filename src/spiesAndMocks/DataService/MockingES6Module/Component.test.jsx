005ccca8b710e3a73df26d548354005dd4047093
sofacles <sofacles@yahoo.com>
2024-10-04 14:07:26 -0700

    can't mock a module

--- src/spiesAndMocks/DataService/MockingES6Module/Component.test.jsx
+++ src/spiesAndMocks/DataService/MockingES6Module/Component.test.jsx
@@ -4,20 +4,22 @@ import { render, screen } from "@testing-library/react";
 import Component from "./Component";
 import { getCatFact } from "./CatService";
 
-jest.mock('./CatService', () => ({
-    __esModule: true, 
-    default: "mockedDefaultExport",
-    getCatFact: jest.fn(() => ({fact: "puny"})),
-  }));
-
-
-
 test("mockify", async () => {
+    const mockCatFact = jest.fn();
 
+    mockCatFact.mockImplementation(() => ({fact: "mocked"}));
+    
+    
+    jest.mock('./CatService', () => ({
+        __esModule: true, 
+        default: "mockedDefaultExport",
+        getCatFact: mockCatFact
+      }));
+    
     
     const element = render(
         <Component />
     );
     const myDiv = await screen.findByTestId("my-div")
-    expect(myDiv).toHaveTextContent("Andromeda");//Fails! value not mocked
+    expect(myDiv).toHaveTextContent("mocked");//Fails! value not mocked
 });
