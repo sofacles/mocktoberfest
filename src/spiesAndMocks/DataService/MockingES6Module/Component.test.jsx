fe371b200455cb5f4a078fce49ae70bf3f40a319
sofacles <sofacles@yahoo.com>
2024-10-06 08:51:20 -0700

    actually mocking a named export from an es6 module

--- src/spiesAndMocks/DataService/MockingES6Module/Component.test.jsx
+++ src/spiesAndMocks/DataService/MockingES6Module/Component.test.jsx
@@ -2,24 +2,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Component from "./Component";
-import { getCatFact } from "./CatService";
+import * as useCatService from "./CatService";
 
 test("mockify", async () => {
-    const mockCatFact = jest.fn();
+    useCatService.getCatFact = jest.fn(() => ({fact: "mocked"}));
 
-    mockCatFact.mockImplementation(() => ({fact: "mocked"}));
-    
-    
-    jest.mock('./CatService', () => ({
-        __esModule: true, 
-        default: "mockedDefaultExport",
-        getCatFact: mockCatFact
-      }));
-    
-    
     const element = render(
         <Component />
     );
     const myDiv = await screen.findByTestId("my-div")
-    expect(myDiv).toHaveTextContent("mocked");//Fails! value not mocked
+    expect(myDiv).toHaveTextContent("mocked");
 });
